30e3c85e3dff912992475503c2eb9dbbd1ddc97c
den8383arfp <den8383arfp@gmail.com>
2020-11-09 18:33:00 +0900

    [add] it can invite user in rest room

---
+++ chat-app/src/components/ChannelEntryButton.vue
@@ -0,0 +1,43 @@
+<template>
+  <input id="input-channel" type="text" v-model="newChannelName"/>
+  <button @click="addChannel">Entry</button>
+</template>
+
+<style>
+#input-channel{
+  margin:0;
+  width:50%;
+}
+</style>
+
+
+
+<script>
+
+
+
+
+
+
+// Initialize Firebase
+
+
+export default {
+  name: "channelCreateButton",
+  props:{
+  },
+  data() {
+    return {
+      newChannelName: ""
+    }
+  },
+  methods: {
+    addChannel(){
+      this.$emit("input-channel-id",this.newChannelName)
+      this.newChannelName = ""
+    },
+  },
+  mounted(){
+  }
+}
+</script>
---
+++ chat-app/src/components/OnlineUsersButton.vue
@@ -0,0 +1,60 @@
+<template>
+  <div v-for="user in users" :key="user.user_id">
+    <span>{{user.email}}</span>
+    <span :class="isOnline(user,connections) ? 'online-icon' : 'offline-icon'"></span>
+    <button @click="inviteThisUser(user)">invite</button>
+  </div>
+<!--  <h2>OnlineUser</h2>
+  <div v-for="user in connections" :key="user.user_id">
+    {{user.user_id}}
+  </div>
+  !-->
+</template>
+
+<style>
+.online-icon{
+  border-radius: 50%;
+  background:lightgreen;
+  display: inline-block;
+  height:0.5em;
+  margin:auto 1em;
+  width:0.5em;
+}
+.offline-icon{
+  border-radius: 50%;
+  background:gray;
+  display: inline-block;
+  height:0.5em;
+  margin:auto 1em;
+  width:0.5em;
+}
+
+</style>
+
+
+
+<script>
+
+
+
+export default {
+  name: "OnlineUsersButton",
+  props:{
+    users: Array,
+    connections: Array
+  },
+  methods: {
+    isOnline(user, connections){
+      if (connections.find(item => item.user_id === user.user_id) != undefined){
+        return true;
+      }
+      else{
+        return false;
+      }
+    },
+    inviteThisUser(user){
+      this.$emit("invite",user)
+    }
+  },
+};
+</script>
